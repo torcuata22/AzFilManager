5cce5731334e915f93c7465dab5fda335bc73fda
torcuata22 <marilynjmarquez@gmail.com>
2023-09-28 15:46:40 -0400

    refactoring

--- afm/files/azure_controller.py
+++ afm/files/azure_controller.py
@@ -1,68 +1,72 @@
-from io import BytesIO
-import environ
 import uuid
 from pathlib import Path
-
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-from azure.storage.blob import BlobClient, BlobServiceClient #BlobClient(account_url: str, container_name: str, blob_name: str,
+from azure.storage.blob import BlobServiceClient
 from django.conf import settings
-
 from . import models
 
+ALLOWED_EXTENSIONS = ['.png', '.jpg', '.svg']
 
-ALLOWED_EXTENSIONS = ['.png', '.jpg', '.svg',]
+def generate_blob_name(file_name):
+    """Generates a unique blob name based on a UUID and file extension."""
+    prefix = uuid.uuid4().hex
+    ext = Path(file_name).suffix
+    return f"{prefix}{ext}"
+
+def upload_file_to_blob(container_name, connection_string, file_path, file_name):
+    try:
+        blob_name = generate_blob_name(file_name)
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        
+        # Upload the file to Azure Blob Storage
+        with open(file_path, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+        
+        return blob_client.url  # Return the URL of the uploaded blob
+    except Exception as e:
+        # Handle any exceptions here, log them, and possibly raise custom exceptions
+        return None  # Or raise an exception depending on your error handling strategy
 
 def check_file_ext(path):
     ext = Path(path).suffix
     return ext in ALLOWED_EXTENSIONS
 
-def list_blobs():
-    blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-    blob_client = blob_service_client.get_blob_client(container = settings.CONTAINER_NAME, blob='blob_name')
-    container_name=settings.CONTAINER_NAME
-    container_client=blob_service_client.get_container_client(container_name)
-    blob_list = container_client.list_blobs(container_name)
-    for blob in blob_list:
-        blob_name = blob.name
-        return blob_name
-    return blob_list
-    
-    
-def download_a_blob(blob_name):
-    blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-    blob_client = blob_service_client.get_blob_client(container = settings.CONTAINER_NAME, blob=blob_name)
-    if not blob_client.exists():
-        return
-    blob_content = blob_client.download_blob()
-    return blob_content
+def list_blobs_in_container(container_name):
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_list = container_client.list_blobs()
+        return [blob.name for blob in blob_list]
+    except Exception as e:
+        # Handle any exceptions here, log them, and possibly raise custom exceptions
+        return []
 
+def download_blob(blob_name):
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
+        blob_client = blob_service_client.get_blob_client(container=settings.CONTAINER_NAME, blob=blob_name)
+        if blob_client.exists():
+            blob_content = blob_client.download_blob()
+            return blob_content
+        else:
+            return None
+    except Exception as e:
+        # Handle any exceptions here, log them, and possibly raise custom exceptions
+        return None
 
-def delete_a_blob(blob_name):
-     blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-     container_client = blob_service_client.get_blob_client(container = settings.CONTAINER_NAME, blob=blob_name)
-     try:
-        if container_client.exists():
-            container_client.delete_blob()
+def delete_blob(blob_name):
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
+        blob_client = blob_service_client.get_blob_client(container=settings.CONTAINER_NAME, blob=blob_name)
+        if blob_client.exists():
+            blob_client.delete_blob()
         else:
             print("Blob does not exist")
-     except Exception as e:
-         print("Failed to delete file")
-    
+    except Exception as e:
+        # Handle any exceptions here, log them, and possibly raise custom exceptions
+        print("Failed to delete file")
+
 def save_file_url_to_db(file_url):
-    new_file = models.File.objects.create(file_url = file_url)
+    new_file = models.File.objects.create(file_url=file_url)
     new_file.save()
     return new_file
-
-def upload_file_to_blob(file_path, file_name):
-    prefix = uuid.uuid4().hex
-    ext=Path(file_name).suffix
-    file_url=f'{prefix}{ext}'
-    blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-    blob_client = blob_service_client.get_blob_client(container = settings.CONTAINER_NAME, blob=file_name)
-    blob_client.upload_blob(file_path, overwrite=True)
-    save_file_url_to_db(blob_client.url)
-    print(f"Uploaded {file_name}")
-
-    
-   
--- afm/files/views.py
+++ afm/files/views.py
@@ -5,70 +5,73 @@ from django.conf import settings
 from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 
-from .azure_controller import ALLOWED_EXTENSIONS,  upload_file_to_blob, download_a_blob, delete_a_blob, list_blobs
-from azure.storage.blob import BlobServiceClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 from uuid import uuid4
 
 from . import models
 from .models import File
-
-from hashlib import new
-from pathlib import Path
-import mimetypes
-
-
-# Create your views here.
+from .azure_controller import (
+    ALLOWED_EXTENSIONS,
+    upload_file_to_blob,
+    download_blob,
+    delete_blob,
+    list_blobs_in_container,
+)
 
 def index(request):
-    return render (request, 'files/index.html')
+    """
+    Render the index page.
+    """
+    return render(request, 'files/index.html')
 
 def list_files(request):
+    """
+    List files from Azure Blob Storage and paginate the results.
+    """
     blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-    container_name=settings.CONTAINER_NAME
-    container_client=blob_service_client.get_container_client(container_name)
-    blob_list = container_client.list_blobs()
-    #for paginator (b/c Azure object not iterable):
-    blob_name_list=[]
-    for blob in blob_list:
-        blob_name = blob.name
-        blob_name_list.append(blob_name)
-    paginator = Paginator(blob_name_list, 10)  #Change number of items per page to a higher number (10? 20?)
+    container_name = settings.CONTAINER_NAME
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = list_blobs_in_container()
+    
+    paginator = Paginator(blob_list, 10)  # Change the number of items per page as needed
     page = request.GET.get('page') 
     blob_name_list = paginator.get_page(page)
-    
   
-
     return render(request, 'files/list_files.html', {'blob_name_list': blob_name_list})
 
 def upload_file(request):
+    """
+    Handle file upload to Azure Blob Storage.
+    """
     if request.method == "POST": 
         files = request.FILES.getlist('files')
         for file in files:
             file_name = file.name
             upload_file_to_blob(file, file_name)    
-            messages.success(request, f"{file.name} was successfully uploaded")
+            messages.success(request, f"{file_name} was successfully uploaded")
     return render(request, 'files/upload_files.html', {})
 
+def delete_file(request, blob_name):
+    """
+    Delete a file from Azure Blob Storage.
+    """
+    try:
+        delete_blob(blob_name)
+        messages.success(request, f"{blob_name} was successfully deleted")
+    except Exception as e:
+        messages.error(request, f"Failed to delete {blob_name}: {str(e)}")
 
-def delete_file(request,blob_name):
-    blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING)
-    container_name=settings.CONTAINER_NAME
-    container_client=blob_service_client.get_container_client(container_name)
-    blob_list=list_blobs()
-    for blob in blob_list:
-        blob_name = blob.name
-    delete_a_blob(blob_name)
-    list_blobs()
-    return render(request,'files/delete_file.html', {"blob_list":blob_list})   #(request,"files/delete_file.html", {'file_id': file_id})
-
-
-    #NOT WORKING YET:
+    return render(request, 'files/delete_file.html', {})
 
 def download_file(request, blob_name):
-    blob_content = download_a_blob(blob_name)
+    """
+    Download a file from Azure Blob Storage.
+    """
+    blob_content = download_blob(blob_name)
     if blob_content:
         response = HttpResponse(blob_content.readall())
         response['Content-Disposition'] = f'attachment; filename={blob_name}'
         messages.success(request, "Your file was successfully downloaded")
         return response
+    
     return render(request, 'files/list_files.html', {})
