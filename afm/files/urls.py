2030b7d1f956bd211881fa047fd58aa2f5c41154
torcuata22 <marilynjmarquez@gmail.com>
2023-09-30 14:46:04 -0400

    locked everything down

--- afm/files/urls.py
+++ afm/files/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -7,13 +8,13 @@ from .views import RegistrationView
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', RedirectView.as_view(url=reverse_lazy('login')), name='index'),
+    path('index/', views.index, name="index"),
     path('upload_file/', views.upload_file, name="upload_file"),
     path('list_files/', views.list_files, name="list_files"),
     path('download_file/<str:blob_name>/', views.download_file, name="download_file"),
     path('delete_file/<str:blob_name>/', views.delete_file, name="delete_file"),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    # path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login' ),
     path('logout/', views.logout_user, name='logout' ),
 
